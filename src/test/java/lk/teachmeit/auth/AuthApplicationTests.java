5cf0d9dde955be16f6b6c35259440cb9a7429d61
Tharuka Lakshan <tharuka@dspcontrols.lk>
2020-09-19 11:40:34 +0530

    Initial commit

---
+++ src/test/java/lk/teachmeit/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.teachmeit.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
